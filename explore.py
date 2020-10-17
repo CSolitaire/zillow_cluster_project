b5ce126da1a04331b82cd73c39ed18787116f5cd
Corey Solitaire <corey.solitaire@gmail.com>
2020-10-17 18:15:45 -0500

    Inital Commit

---
+++ explore.py
@@ -0,0 +1,131 @@
+import pandas as pd
+import numpy as np
+import os
+from env import host, user, password
+import scipy as sp 
+from env import host, user, password
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, QuantileTransformer, PowerTransformer, RobustScaler, MinMaxScaler
+from math import sqrt
+from scipy import stats
+from sklearn.cluster import KMeans
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+################## Explore ##############################################################################################
+
+def nulls_by_col(df):
+    num_missing = df.isnull().sum()
+    rows = df.shape[0]
+    pct_missing = num_missing / rows
+    cols_missing = pd.DataFrame({'number_missing_rows': num_missing, 'percent_rows_missing': pct_missing})
+    return cols_missing
+
+def nulls_by_row(df):
+    num_cols_missing = df.isnull().sum(axis=1)
+    pct_cols_missing = df.isnull().sum(axis=1)/df.shape[1]*100
+    rows_missing = pd.DataFrame({'num_cols_missing': num_cols_missing, 'pct_cols_missing': pct_cols_missing}).reset_index().groupby(['num_cols_missing','pct_cols_missing']).count().rename(index=str, columns={'index': 'num_rows'}).reset_index()
+    return rows_missing 
+
+def df_summary(df):
+    '''
+    This function returns all the summary information of the dataframe
+    '''
+    print('The shape of the df:') 
+    print(df.shape)  # df shape (row/column)
+    print('\n')
+    print('Columns, Non-Null Count, Data Type:')
+    print(df.info())      # Column, Non Null Count, Data Type
+    print('\n')
+    print('Summary statistics for the df:') 
+    print(df.describe())             # Summary Statistics on Numeric Data Types
+    print('\n')
+    print('Number of NaN values per column:') 
+    print(df.isna().sum())           # NaN by column
+    print('\n')
+    print('Number of NaN values per row:')  
+    print(df.isnull().sum(axis=1))   # NaN by row
+    for col in df.columns:
+        print('-' * 40 + col + '-' * 40 , end=' - ')
+        display(df[col].value_counts(dropna=False).head(10))
+        #display(df_resp[col].value_counts())  # Displays all Values, not just First 10
+
+# df_summary(df) | To call function
+
+################## Outliers and IQR #################### 
+
+def get_upper_outliers(s, k):  
+    '''
+    Given a series and a cutoff value, k, returns the upper outliers for the
+    series.
+    The values returned will be either 0 (if the point is not an outlier), or a
+    number that indicates how far away from the upper bound the observation is.
+    '''
+    q1, q3 = s.quantile([.25, .75])
+    iqr = q3 - q1
+    upper_bound = q3 + k * iqr
+    return s.apply(lambda x: max([x - upper_bound, 0]))
+
+def add_upper_outlier_columns(df, k): # Call This Function First
+    '''
+    Add a column with the suffix _outliers for all the numeric columns
+    in the given dataframe.
+    '''
+    # outlier_cols = {col + '_outliers': get_upper_outliers(df[col], k)
+    #                 for col in df.select_dtypes('number')}
+    # return df.assign(**outlier_cols)
+    for col in df.select_dtypes('number'):
+        df[col + '_outliers'] = get_upper_outliers(df[col], k)
+    return df
+
+
+###################################### STEP #1 
+# In the next cell type the following
+'''
+'''
+#This text prints information regrding the outlier columns created 
+'''
+wrangle.add_upper_outlier_columns(df, k=3)    
+outlier_cols = [col for col in df if col.endswith('_outliers')]
+for col in outlier_cols:
+    print('~~~\n' + col)
+    data = df[col][df[col] > 0]
+    print(data.describe())
+'''
+###################################### STEP #2
+
+# Print this code to remove colums in dataframe
+'''
+X_train_explore.drop([x for x in df if x.endswith('_outliers')], 1, inplace = True)
+
+'''
+
+##################################################################################################################
+
+def elbow_plot(X_train_scaled, cluster_vars):
+    # elbow method to identify good k for us
+    ks = range(2,20)
+    
+    # empty list to hold inertia (sum of squares)
+    sse = []
+
+    # loop through each k, fit kmeans, get inertia
+    for k in ks:
+        kmeans = KMeans(n_clusters=k)
+        kmeans.fit(X_train_scaled[cluster_vars])
+        # inertia
+        sse.append(kmeans.inertia_)
+
+    print(pd.DataFrame(dict(k=ks, sse=sse)))
+
+    # plot k with inertia
+    plt.plot(ks, sse, 'bx-')
+    plt.xlabel('k')
+    plt.ylabel('SSE')
+    plt.title('Elbow method to find optimal k')
+    plt.show()
+
+##################################################################################################################
